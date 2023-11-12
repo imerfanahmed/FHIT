1fc9c7c02db95a5bf86575253b582c0a134208f6
Erfan Ahmed Siam <erfan.siam98@gmail.com>
2023-11-12 12:56:45 +0600

    Chore:Ide-helper

--- app/Models/Record.php
+++ app/Models/Record.php
@@ -5,6 +5,33 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Record
+ *
+ * @property int $id
+ * @property string $business_name
+ * @property int $income
+ * @property int $expense
+ * @property int $debts
+ * @property int $assets
+ * @property float $financial_health_score
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\RecordFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Record newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Record newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Record query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Record whereAssets($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Record whereBusinessName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Record whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Record whereDebts($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Record whereExpense($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Record whereFinancialHealthScore($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Record whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Record whereIncome($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Record whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Record extends Model
 {
     use HasFactory;
